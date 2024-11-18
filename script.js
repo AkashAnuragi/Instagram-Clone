225be6a1b9d6b8fc0de181e1e8766052d5512f72
AKASH ANURAGI <136264753+AkashAnuragi@users.noreply.github.com>
2024-11-18 21:30:27 +0530

    Instagram Clone

---
+++ script.js
@@ -0,0 +1,14 @@
+const dark = document.getElementById("clicked");
+const white = document.getElementById("white-color");
+const lol = document.getElementById("white-2color");
+dark.onclick = function (){
+    document.body.classList.toggle("dark-mode");
+    if(document.body.classList.contains("dark-mode")){
+        white.style.filter = "brightness(5)";
+        lol.style.filter = "brightness(5)";
+    }
+    else{
+        white.style.filter = "none";
+        lol.style.filter = "none";
+    }
+}
